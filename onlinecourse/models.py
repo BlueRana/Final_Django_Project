0ccac7650093526b65e041627d664188dfbefb02
BlueRana <bluefrog79sl@gmail.com>
2025-09-26 12:29:02 -0400

    Finished Project

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,7 +102,7 @@ class Question(models.Model):
 
     def __str__(self):
         return "Question: " + self.content
-        
+
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
@@ -123,3 +123,4 @@ class Choice(models.Model):
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+    
